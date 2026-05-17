ff7a262df56fb08025aa01c857b35f61b73fcfab
Harshal <gahlotharshal@gmail.com>
2026-05-17 14:22:46 +0530

    changed legacy <BrowserRouter> to mordern <createBrowserRouter> in order to use built-in <ScrollRestoration> on each page when coming back to it using navigate(-1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,55 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LandingPage from "./pages/landingpage";
-import SetupPage from "./pages/setuppage";
-import Dashboard from "./pages/dashboardpage";
-import PublicPage from "./pages/publicpage";
-import Singlepostpage from "./pages/singlepostpage";
+import LandingPage from "./pages/landingPage";
+import SetupPage from "./pages/setupPage";
+import Dashboard from "./pages/dashboardPage";
+import PublicPage from "./pages/publicPage";
+import SinglePostPage from "./pages/singlePostPage";
+import {
+	BrowserRouter,
+	Routes,
+	Route,
+	Outlet,
+	ScrollRestoration,
+	createBrowserRouter,
+	RouterProvider,
+} from "react-router-dom";
 
-function App() {
+function RootLayout() {
 	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path="/" element={<LandingPage />}></Route>
-				<Route path="/setup" element={<SetupPage />}></Route>
-				<Route path="/dashboard" element={<Dashboard />}></Route>
-				<Route path="/:username/scroll/:post_id" element={<Singlepostpage />}></Route>
-				<Route path="/:username/:category" element={<PublicPage />}></Route>
-				<Route path="/:username" element={<PublicPage />}></Route>
-			</Routes>
-		</BrowserRouter>
+		<div>
+			<Outlet />
+			<ScrollRestoration />
+		</div>
 	);
 }
 
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <RootLayout />,
+		children: [
+			{ path: "/", element: <LandingPage /> },
+			{ path: "/setup", element: <SetupPage /> },
+			{ path: "/dashboard", element: <Dashboard /> },
+			{ path: "/:username/scroll/:post_id", element: <SinglePostPage /> },
+			{ path: "/:username/:category", element: <PublicPage /> },
+			{ path: "/:username", element: <PublicPage /> },
+		],
+	},
+]);
+
+function App() {
+	return <RouterProvider router={router} />;
+}
+
 export default App;
+
+// <BrowserRouter>
+// 	<Routes>
+// 		<Route path="/" element={<LandingPage />}></Route>
+// 		<Route path="/setup" element={<SetupPage />}></Route>
+// 		<Route path="/dashboard" element={<Dashboard />}></Route>
+// 		<Route path="/:username/scroll/:post_id" element={<SinglePostPage />}></Route>
+// 		<Route path="/:username/:category" element={<PublicPage />}></Route>
+// 		<Route path="/:username" element={<PublicPage />}></Route>
+// 	</Routes>
+// </BrowserRouter>
